afbe4a97cdd31f71fd142d056356003ae317d79b
Meqdad <mkh.pro@gmail.com>
2023-02-07 08:59:28 +0330

    mm

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -34,8 +34,8 @@ values ('30004', 'Work', '02123456789', '20002');
 insert into ENTRY
 values ('30005', 'Mobile', '09919876543', '20003');
 
-insert into ENTRY
-values ('30006', 'HomeFax', '02155667788', '20004');
+/*insert into ENTRY
+values ('30006', 'HomeFax', '02155667788', '20004');*/
 
 insert into CONTACT_LABEL
 values (20001, 10002);
